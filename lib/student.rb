b8ded7dd23a443f2b3cfe8a37945c2dd47095bfd
Allison Howard <alliemhow@gmail.com>
2019-05-08 09:20:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|k, v| self.send(("#{k}="), v)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -17,6 +18,6 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
 end
