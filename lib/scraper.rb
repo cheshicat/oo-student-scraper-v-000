d7507b0e82712610b411fd4d835ed9fde79877cb
Allison Howard <alliemhow@gmail.com>
2019-05-08 09:03:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,8 +39,8 @@ class Scraper
         profile[:blog] = link
       end
     end
-    profile[:profile_quote] = page.css("div.vitals-text-container div.profile-quote").text
-    profile[:bio] = page.css("div.details-container div.description-holder p").text
+    profile[:profile_quote] = page.css(".vitals-text-container div.profile-quote").text
+    profile[:bio] = page.css(".details-container div.description-holder p").text
     profile
   end
 
