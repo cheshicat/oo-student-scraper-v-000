458455975c07da1e080a7f7323b99664afad6aec
Allison Howard <alliemhow@gmail.com>
2019-05-08 06:45:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     index = Nokogiri::HTML(open(index_url))
     students = {}
+    :name, :location and :profile_url.
+    :name, :location and :profile_url.
+
   end
 
   def self.scrape_profile_page(profile_url)
